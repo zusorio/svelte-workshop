868a41f2052d1e26d20e05d1a1c7df68f4175b0c
pr070 <pr070@hdm-stuttgart.de>
2024-01-29 14:26:08 +0100

    add animals js to lib

---
+++ exercises-sveltekit-01/src/lib/animals.js
@@ -0,0 +1,32 @@
+export const animals = [
+    {
+        id: 1,
+        name: "Löwe",
+        content: "Der Löwe ist der König der Tiere. Er lebt hauptsächlich in Afrika und jagt in Rudeln.",
+        imageURL: "URL_zum_Bild_des_Löwen"
+    },
+    {
+        id: 2,
+        name: "Elefant",
+        content: "Der Elefant ist das größte Landtier. Er hat einen Rüssel, der zum Greifen und Trinken verwendet wird.",
+        imageURL: "URL_zum_Bild_des_Elefanten"
+    },
+    {
+        id: 3,
+        name: "Giraffe",
+        content: "Die Giraffe hat den längsten Hals aller Tiere. Sie kann bis zu 6 Meter hoch werden und frisst Blätter von Bäumen.",
+        imageURL: "URL_zum_Bild_der_Giraffe"
+    },
+    {
+        id: 4,
+        name: "Pinguin",
+        content: "Der Pinguin kann nicht fliegen, sondern nur schwimmen. Er lebt hauptsächlich in der Antarktis und hat eine wasserabweisende Federkleid.",
+        imageURL: "URL_zum_Bild_des_Pinguins"
+    },
+    {
+        id: 5,
+        name: "Känguru",
+        content: "Das Känguru kann große Sprünge machen. Es lebt hauptsächlich in Australien und hat einen Beutel, in dem es seine Jungen trägt.",
+        imageURL: "URL_zum_Bild_des_Kängurus"
+    }
+];
